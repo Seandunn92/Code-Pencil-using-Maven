62212db13788bfe7b893536ab3ac75d37e765996
Seandunn92 <seandunn92@gmail.com>
2018-07-11 23:50:40 -0400

    First commit, Maven Quickstart

---
+++ src/main/java/com/sean/dunn/App.java
@@ -0,0 +1,16 @@
+package com.sean.dunn;
+import java.util.Scanner;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+    public static void main( String[] args )
+    {
+        Scanner input =new Scanner(System.in);
+        int i = input.nextInt();
+        System.out.println( "Hello World! "+ i );
+    }
+}
