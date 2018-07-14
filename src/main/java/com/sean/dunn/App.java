44711c3ea2012f8e4e9c5802f85c962726e380d1
Seandunn92 <seandunn92@gmail.com>
2018-07-13 20:35:06 -0400

    Added Write to the App

--- src/main/java/com/sean/dunn/App.java
+++ src/main/java/com/sean/dunn/App.java
@@ -8,25 +8,33 @@ public class App
 {
 
     private static CodePencil appPencil;
+    private static Scanner scanner;
     public static void main( String[] args )
     {
-        Scanner scanner= new Scanner(System.in);
+        scanner= new Scanner(System.in);
         appPencil= new CodePencil();
        int input = -1;
        do {
+           System.out.println();
            System.out.println("**********************************************************************************");
            System.out.println("Welcome to the Code Pencil menu: Please enter in the option number you would like");
            System.out.println("-1 : Quit");
-           System.out.println("1 : Display my Paper");
+           System.out.println("1  : Display my Paper");
+           System.out.println("2  : Write");
            System.out.println("**********************************************************************************");
            input = scanner.nextInt();
            switch(input){
-               case 1: DisplayPaper();
+               case 1: DisplayPaper();break;
+               case 2: Write(); break;
            }
        }while (input>-1);
     }
 
     private static void DisplayPaper() {
         System.out.println(appPencil.getPaper());
+    }
+    private static void Write() {
+        scanner.nextLine();
+        appPencil.write(scanner.nextLine());
     }
 }
