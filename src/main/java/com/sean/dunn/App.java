b1e739fc20a323f998425b7fb5e275fb1d5008c4
Seandunn92 <seandunn92@gmail.com>
2018-07-13 20:20:49 -0400

    Adding Display Paper to App

--- src/main/java/com/sean/dunn/App.java
+++ src/main/java/com/sean/dunn/App.java
@@ -7,15 +7,26 @@ import java.util.Scanner;
 public class App 
 {
 
+    private static CodePencil appPencil;
     public static void main( String[] args )
     {
         Scanner scanner= new Scanner(System.in);
-       CodePencil appPencil= new CodePencil();
+        appPencil= new CodePencil();
        int input = -1;
        do {
-           System.out.println("Welcome to the Code Pencil menu: Choose an option");
-           System.out.println("-1: Quit");
+           System.out.println("**********************************************************************************");
+           System.out.println("Welcome to the Code Pencil menu: Please enter in the option number you would like");
+           System.out.println("-1 : Quit");
+           System.out.println("1 : Display my Paper");
+           System.out.println("**********************************************************************************");
            input = scanner.nextInt();
+           switch(input){
+               case 1: DisplayPaper();
+           }
        }while (input>-1);
+    }
+
+    private static void DisplayPaper() {
+        System.out.println(appPencil.getPaper());
     }
 }
